eabc83c236862211797bf107db5b95b4cd773d17
Edgar Sun <esun@voteb.com>
2022-03-07 20:35:03 +0800

    fixed missing packages

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
     name='compipe',         # How you named your package folder (MyLib)
-    packages=['compipe'],   # Chose the same as "name"
-    version='0.1',      # Start with a small number and increase it with every change you make
+    version='0.1.2',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='a lite version of the command wrapper for python',   # Give a short description about your library
     author='esunvoteb',                   # Type in your name
     author_email='esun@voteb.com',      # Type in your E-Mail
     url='https://github.com/ImagineersHub/compipe',   # Provide either the link to your github or to your website
-    download_url='https://github.com/ImagineersHub/compipe/archive/v_01.tar.gz',    # I explain this later on
+    # download_url='https://github.com/ImagineersHub/compipe/archive/v_01.tar.gz',    # I explain this later on
     keywords=['python', 'command', 'commandlet', 'cmd', 'cmdlet'],   # Keywords that define your package best
+    packages=find_packages(),
     install_requires=[            # I get to this in a second
         'requests>=2.27.*',
         'cryptography>=36.*',
